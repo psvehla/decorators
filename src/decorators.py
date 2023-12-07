b8c3faac0cd72abe7672fd15b54cd35eb76650e5
psvehla <psvehla@hotmail.com>
2023-12-08 00:26:18 +1100

    Tidied up some confusion

--- src/decorators.py
+++ src/decorators.py
@@ -1,8 +1,10 @@
 """My decorators."""
+import functools
 
 
 def do_twice(func):
     """Do twice."""
+    @functools.wraps(func)
     def wrapper_do_twice(*args, **kwargs):
         func(*args, **kwargs)
         return func(*args, **kwargs)
