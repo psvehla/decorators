d11c13c98d5cbabc72fd2dcc6e2e9a4973c0aafd
psvehla <psvehla@hotmail.com>
2023-12-09 21:07:30 +1100

    Added decorator boilerplate.

--- src/decorators.py
+++ src/decorators.py
@@ -9,3 +9,14 @@ def do_twice(func):
         func(*args, **kwargs)
         return func(*args, **kwargs)
     return wrapper_do_twice
+
+
+def decorator(func):
+    """Boilerplate for a decorator."""
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        # Do somehting before
+        value = func(*args, **kwargs)
+        # Do somehting before
+        return value
+    return wrapper_decorator
